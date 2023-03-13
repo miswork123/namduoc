030c8f61f4d93fff56a3060bece648dce605c954
caonguyen <caonguyenvhit@gmail.com>
2023-03-13 20:39:16 +0700

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+let menuIcon = document.querySelector('.sup-menu');
+let menuMobile = document.querySelector('.header-list-menu');
+
+menuIcon.addEventListener('click', function() {
+    this.classList.toggle('active');
+    menuMobile.classList.toggle('show');
+});
