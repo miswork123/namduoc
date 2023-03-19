73fa78e6c69aae77f70fc153706b412709242831
caonguyen <caonguyenvhit@gmail.com>
2023-03-19 20:00:50 +0700

    fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,25 @@
 let menuIcon = document.querySelector('.sup-menu');
 let menuMobile = document.querySelector('.header-list-menu');
-
+    
 menuIcon.addEventListener('click', function() {
     this.classList.toggle('active');
     menuMobile.classList.toggle('show');
   
 });
 
-let headerSearch = document.querySelector('.header__search');
+let headerSearch = document.querySelector('.tim-kiem');
 let searchGoogle = document.querySelector('.search--google');
 
 headerSearch.addEventListener('click', function()
 {
     searchGoogle.classList.toggle('open');
-    if(searchGoogle){
-        console.log('dung');
-
-    }else{
-        console.log('sai')
-    }
+    
 }); 
+
+var myString = 'Le cao nguyen'
+console.log(myString.length)
+console.log(myString.indexOf('caoa'))
+console.log(myString.replace('cao', 'Phong'))
+console.log(myString.toUpperCase())
+console.log(myString.toLowerCase())
+console.log(myString.split(' '))
